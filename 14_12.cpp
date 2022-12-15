f509cb9a52119b0d46ecbd3a54ec23a1e99638d3
Khoa la tao <anhkhoa.24052003@gmail.com>
2022-12-15 15:48:31 +0700

    update <1> 14_12.cpp file!

--- 14_12.cpp
+++ 14_12.cpp
@@ -38,13 +38,23 @@ public:
     }
     void dac_diem()
     {
-        std::cout<<"hahahahaha"<<std::endl;
+        std::cout << "hahahahaha" << std::endl;
+    }
+    float get_weight()
+    {
+        return weight;
+    }
+    float get_height()
+    {
+        return height;
     }
 
 protected:
     std::string name;
     int age;
     std::string gioi_tinh;
+    float weight;
+    float height;
 };
 class Sv_gioi : public sinh_vien
 {
@@ -52,12 +62,12 @@ public:
     Sv_gioi(std::string n, int a, std::string gt) : sinh_vien(n, a, gt) {}
     void sua_do()
     {
-        std::cout<<name<<" said: ";
+        std::cout << name << " said: ";
         std::cout << "'Bo may hoc gioi vai lon!'" << std::endl;
     }
     void dac_diem()
     {
-        std::cout<<"hehehehehhe"<<std::endl;
+        std::cout << "hehehehehhe" << std::endl;
     }
 
 private:
@@ -68,41 +78,45 @@ public:
     Sv_ngu(std::string n, int a, std::string gt) : sinh_vien(n, a, gt) {}
     void thua_ly_do()
     {
-        std::cout<<name<<" said: ";
+        std::cout << name << " said: ";
         std::cout << "'Dcm de kho vai cc'" << std::endl;
     }
 
 private:
 };
 
+class maykhamsuc
+{
+private:
+    int age;
+    float weight;
+    float height;
+public:
+    ~maykhamsuc(){}
+    void haha(sinh_vien *N)
+    {
+        age = N->get_age();
+        weight = N ->get_weight();
+        height = N ->get_height();
+
+        th();
+    }
+    void th()
+    {
+        if(age >= 18)
+        {
+            std::cout<<"cau thu chuyen nghiep"<<std::endl;
+        }
+        else
+        {
+            std:: cout <<"cau thu tre"<<std::endl;
+        }
+    }
+
+};
 
 int main()
 {
-    
-    //========khoa da update========
-    
-    
-    /*// sinh_vien *khoalatao = new sinh_vien("Huynh Anh Khoa", 19, "Nam");
-    // std::cout<<khoalatao->get_name();
 
-    // delete khoalatao;
-    // Sv_gioi *khoalatao = new Sv_gioi("Huynh Anh Khoa", 19, "Nam");
-    // khoalatao->sua_do();
-    // khoalatao->dac_diem();
-
-    // std::cout<<std::endl;
-
-    // Sv_ngu *haha = new Sv_ngu("Le Thi Ngu", 19, "Deo biet");
-    // haha->thua_ly_do();
-    // haha->dac_diem();
-
-    // std::cout<<std::endl;
-    // delete khoalatao;
-    // delete haha;*/
-    
-    //helo tao hoc git
-
-    //
     return 0;
-
 }
